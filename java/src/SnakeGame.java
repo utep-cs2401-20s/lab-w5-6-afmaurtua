e029bab1a77e75589ae1b3df61d263054bc73d42
aliciamaurtua <afmaurtua@miners.utep.edu>
2020-02-19 14:47:49 -0700

    Working on understanding the algorithms

---
+++ java/src/SnakeGame.java
@@ -0,0 +1,63 @@
+public class SnakeGame {
+    private boolean[][] game;
+    private int[] headPosition;
+
+    private static int[] headPosition(){
+
+
+    }
+
+    private static int exhaustiveChecks(){
+        int count = 0;
+
+    }
+
+    private static int recursiveCheck(){
+
+    }
+
+    //default constructors
+    public SnakeGame(){
+        int[][] empty = new int[][];
+
+    }
+
+    //constructors
+    public SnakeGame(int x, int y, boolean [][] head){
+
+    }
+
+    public int[] findTailExhaustive(){
+        for(int i = 0; i < headPosition.length; i++){
+            for(int k = 0; k < headPosition.length; k++){
+
+            }
+        }
+
+    }
+
+    public int[] findTailRecursive(){
+
+    }
+
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
+
+    }
+
+    private void resetCounters(){
+
+    }
+
+    private static int getRecursiveChecks(){
+
+
+    }
+
+    private static int getExhaustiveChecks(){
+
+    }
+
+
+
+
+}
