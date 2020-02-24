6a302e9521fbb3c050fe2be875946f3a99cddfc3
aliciamaurtua <afmaurtua@miners.utep.edu>
2020-02-24 14:48:39 -0700

    added where the head is

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,37 +1,52 @@
 public class SnakeGame {
     private boolean[][] game;
     private int[] headPosition;
-
-    private static int[] headPosition(){
-
-
-    }
-
-    private static int exhaustiveChecks(){
-        int count = 0;
-
-    }
-
-    private static int recursiveCheck(){
-
-    }
+    private static int exhaustiveChecks;
+    private static int recursiveCheck;
 
     //default constructors
     public SnakeGame(){
-        int[][] empty = new int[][];
-
+        int[] gameboard = new int[1];
     }
 
     //constructors
-    public SnakeGame(int x, int y, boolean [][] head){
-
+    public SnakeGame(int x, int y, boolean [][] g){
+       game = g;
+       headPosition = new int[]{x, y};
+       headPosition[0] = x;
+       headPosition[1] = y;
+        //head[x][y] = true;
     }
 
     public int[] findTailExhaustive(){
-        for(int i = 0; i < headPosition.length; i++){
-            for(int k = 0; k < headPosition.length; k++){
-
+        int[] result = new int[3];
+        int count = 0;
+        for(int i = 0; i < game.length; i++){ //going through the board
+            for(int k = 0; k < game[i].length; k++){
+               if(game[i][k] == true){ //checking to see if the tail is there
+                   if(i == headPosition[0] && k == headPosition[1]){ //this is the head
+                       continue;
+                   }
+                   if(neighbors(i, k) == 1){ //checking
+                       result[0] = i;
+                       result[1] = k;
+                   }
+               }
             }
+        }
+        result[2] = length();
+    }
+
+    private int length() {
+        int length = 0;
+        return length;
+    }
+
+    public int neighbors(int i, int k){
+       int length = 0;
+       //top
+        if (){
+
         }
 
     }
@@ -49,12 +64,11 @@ public class SnakeGame {
     }
 
     private static int getRecursiveChecks(){
-
-
+       return RecursiveChecks;
     }
 
     private static int getExhaustiveChecks(){
-
+        return getExhaustiveChecks();
     }
 
 
